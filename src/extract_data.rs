afe98ef50e1d94b7094efa675e313e80347fa226
krut_ni <nils.kruthoff@dlr.de>
2025-01-20 19:07:01 +0100

    included file-format crate

---
+++ src/extract_data.rs
@@ -0,0 +1,17 @@
+﻿use std::error::Error;
+use file_format::{FileFormat, Kind};
+use std::fs::exists;
+
+pub fn extract_data(file_path: &str) -> Result<String, Box<dyn Error>> {
+    if let Err(_) = exists(file_path) {
+        return Err(Box::from("File does not exist."));
+    }
+
+    let fmt = FileFormat::from_file(file_path)?;
+    let kind = fmt.kind();
+    println!("kind: {:?}", &kind);
+    match kind {
+        Kind::Other => Ok(file_path.to_string()),
+        _ => { Err(Box::from("File does not support kind.")) }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod extract_data;
+
+use extract_data::*;
 fn main() {
-    println!("Hello, world!");
+    let path = "C:/Users/krut_ni/RustroverProjects/AIStudioDataPreparation/test-data/AuthorController.cs";
+
+    let fmt_result = extract_data(path);
+    match fmt_result {
+        Ok(data) => {println!("{}", data)},
+        Err(e) => {println!("{}", e)}
+    }
 }
